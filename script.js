d51df6721ef9bf3e579e2b4d9606b92caed0fe8e
jprimaki <43557280+jprimaki@users.noreply.github.com>
2018-09-26 01:36:47 -0300

    Update

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 window.onload = function() {
     controlador = new Controlador();
-    controlador.Desenhar();
+    controlador.Desenhar();    
 };
 
 class Cubo {
@@ -28,6 +28,18 @@ class Controlador {
         }
     }
 
+    set alterarEstilo (estilo = ""){
+        this.retornarCanvas.fillStyle = estilo;
+    }
+
+    get larguraCanvas (){
+        return this.retornarCanvas.canvas.clientWidth
+    }
+
+    get alturaCanvas (){
+        return this.retornarCanvas.canvas.clientHeight;
+    }
+
     get retornarCanvas() {
         return this.canvas;
     }
@@ -36,58 +48,77 @@ class Controlador {
         return Math.floor(Math.random() * (maximo - minimo + 1)) + minimo;
     }
 
-    coraleatoria() {
+    corAleatoria() {
         return 'hsl(' + 360 * Math.random() + ', 50%, 50%)';
     }
 
-    desenharNumero (cubo) {       
-        cubo.quantidade = this.randomico(1,3);
+    desenharNumero (cubo) {               
         let padrao = 15;
-        this.retornarCanvas.fillStyle = "#ffffff";
+
+        cubo.quantidade = this.randomico(1,3);
+        this.alterarEstilo = "#ffffff";
         this.retornarCanvas.fillText(cubo.quantidade, cubo.x + (cubo.w / 2), padrao + cubo.y);
     }
 
     desenharCubos () {                  
         let quantidadeCubos = this.randomico(15,15);
-        let espaco = 5;
+        let espacoEntreCubo = 5;
+        let borda = 5;
         let x = 0;
         let y = 0;
         let w = 50;
         let h = 15;
         let cubo = null;
-        this.retornarCanvas.canvas.width = (quantidadeCubos * (w + espaco)) + 5; 
+        this.retornarCanvas.canvas.width = (quantidadeCubos * (w + espacoEntreCubo)) + borda; 
 
         for (var i = 0; i < quantidadeCubos; i++) {
             x = 0;
             for (var z = 0; z < quantidadeCubos; z++) {
-                this.retornarCanvas.fillStyle = this.coraleatoria();
-                this.retornarCanvas.fillRect (x + espaco, y + espaco, w, h);                               
+                this.alterarEstilo = this.corAleatoria();
+                this.retornarCanvas.fillRect (x + espacoEntreCubo, y + espacoEntreCubo, w, h);                               
                 cubo = new Cubo(x, y, w, h);
                 this.listaCubo.push(cubo);  
 
                 this.desenharNumero(cubo);
                 
-                x += w + espaco;
+                x += w + espacoEntreCubo;
             }       
-            y += h + espaco;       
+            y += h + espacoEntreCubo;       
         }
 
     }    
 
-    desenharBase () {                                  
+    desenharBase () {         
+        let widthBase = 150;                           
+        let heightBase = 10;
+        let borda = 50;
+        this.alterarEstilo = "#FFF";
+        this.retornarCanvas.fillRect (this.larguraCanvas / 2 - (widthBase / 2), this.alturaCanvas - borda, widthBase, heightBase);                                    
+    }
+
+    desenharBola (){
         this.retornarCanvas.beginPath();
-        this.retornarCanvas.lineCap = "round";
-        this.retornarCanvas.lineWidth = 10;
-        this.retornarCanvas.moveTo(this.retornarCanvas.canvas.clientWidth / 2, this.retornarCanvas.canvas.clientHeight / 2);
-        this.retornarCanvas.lineTo(950, 156);
-        this.strokeStyle = "#FF0000";
-        this.retornarCanvas.stroke();      
+        this.alterarEstilo ="#ff0000";        
+        this.retornarCanvas.arc(this.larguraCanvas / 2, this.alturaCanvas / 1.5 ,20,0,Math.PI*2,true); 
+        this.retornarCanvas.closePath();
+        this.retornarCanvas.fill();
+    }
+
+    desenharStart (){
+        let alfa = 1;
+        let texto = "PRESS ENTER TO START";
+        this.alterarEstilo = "rgba(255, 255, 255, " + alfa + ")";
+        this.retornarCanvas.textAlign = "center";
+        this.retornarCanvas.font = "35px Georgia";
+        this.retornarCanvas.fillText(texto, this.larguraCanvas/2, this.alturaCanvas/2);      
+        //let larguraTexto = this.retornarCanvas.measureText(texto).width;        
     }
 
     Desenhar (){        
         this.desenharCubos();  
-        //this.desenharBase();
-        //this.DesenharBola;     
+        this.desenharBase();
+        this.desenharBola();     
+        this.desenharStart();
     }
     
 }
